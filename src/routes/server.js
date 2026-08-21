f3a3cb9a475180b9a0f6371914db9668fdd41312
Mihir Patel <mk04patel@gmail.com>
2026-08-21 00:13:49 -0500

    Imported auth routes, limited json request body size, provided server error

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import authRoutes from './authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json({ limit: '10kb' }));
+app.use('/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(error.status || 500).json({ error: 'Internal server error' });
 });
 
 app.listen(PORT, () => {
